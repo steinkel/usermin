7236cc261063811b4412d2e4243e61a8b2d2445d
steinkel <steinkel@gmail.com>
2012-02-11 19:19:35 +0000

    test case added for allow=0 permission

--- Test/Case/Controller/Component/RoleAuthorizeTest.php
+++ Test/Case/Controller/Component/RoleAuthorizeTest.php
@@ -92,10 +92,19 @@ class RoleAuthorizeTest extends CakeTestCase {
     public function testSettingsAuthorizeAllIsAuthorized() {
         $user = array('username' => 'otheruser', 'umrole_id' => 'nogroup');
         $request = new CakeRequest('/othercontroller/otheraction');
-        
+
         $this->auth = new RoleAuthorize($this->components, array('debug' => true, 'authorizeAll' => true));
-        
+
         $this->assertTrue($this->auth->authorize($user, $request));
     }
 
+    public function testAuthorizeFailureNotAllowed() {
+        $user = array('username' => 'user-0', 'umrole_id' => '00000000-0000-0000-0000-000000000000');
+
+        $request = new CakeRequest('/controllernotallowed/actionnotallowed');
+        $this->assertFalse($this->auth->authorize($user, $request));
+    }
+
+    //TODO: Tests to cover '*' rules    
+    //TODO: Tests to cover not authorized roles
 }
--- Test/Fixture/UmpermissionFixture.php
+++ Test/Fixture/UmpermissionFixture.php
@@ -1,4 +1,5 @@
 <?php
+
 /* Umpermission Fixture generated on: 2012-02-10 21:52:50 : 1328907170 */
 
 /**
@@ -7,40 +8,52 @@
  */
 class UmpermissionFixture extends CakeTestFixture {
 
-/**
- * Fields
- *
- * @var array
- */
-	public $fields = array(
-		'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'umrole_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'umuser_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'plugin' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'controller' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'action' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'params' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
-		'allowed' => array('type' => 'boolean', 'null' => true, 'default' => '1', 'collate' => NULL, 'comment' => ''),
-		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_general_ci', 'engine' => 'InnoDB')
-	);
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    public $fields = array(
+        'id' => array('type' => 'string', 'null' => false, 'default' => NULL, 'length' => 36, 'key' => 'primary', 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'umrole_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'umuser_id' => array('type' => 'string', 'null' => true, 'default' => NULL, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'plugin' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'controller' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'action' => array('type' => 'string', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'params' => array('type' => 'text', 'null' => true, 'default' => NULL, 'collate' => 'latin1_swedish_ci', 'comment' => '', 'charset' => 'latin1'),
+        'allowed' => array('type' => 'boolean', 'null' => true, 'default' => '1', 'collate' => NULL, 'comment' => ''),
+        'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
+        'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_general_ci', 'engine' => 'InnoDB')
+    );
 
-/**
- * Records
- *
- * @var array
- */
-	public $records = array(
-                // group-0 allowed to /pages/display
-		array(
-			'id' => '00000000-0000-0000-0000-000000000000',
-			'umrole_id' => '00000000-0000-0000-0000-000000000000',
-			'umuser_id' => 'not-used',
-			'plugin' => '',
-			'controller' => 'controllerauthorized',
-			'action' => 'actionauthorized',
-			'params' => '',
-			'allowed' => 1
-		),
-	);
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = array(
+        // group-0 allowed to /controllerauthorized/actionauthorized
+        array(
+            'id' => '00000000-0000-0000-0000-000000000000',
+            'umrole_id' => '00000000-0000-0000-0000-000000000000',
+            'umuser_id' => 'not-used',
+            'plugin' => '',
+            'controller' => 'controllerauthorized',
+            'action' => 'actionauthorized',
+            'params' => '',
+            'allowed' => 1
+        ),
+        // group-0 NOT allowed to /controllernotallowed/actionnotallowed
+        array(
+            'id' => '11111111-1111-1111-1111-111111111111',
+            'umrole_id' => '00000000-0000-0000-0000-000000000000',
+            'umuser_id' => 'not-used',
+            'plugin' => '',
+            'controller' => 'controllernotallowed',
+            'action' => 'actionnotallowed',
+            'params' => '',
+            'allowed' => 0
+        ),
+    );
+
 }
